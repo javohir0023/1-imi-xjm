787d8553084a0c23d454c81db53be0bb161e283d
m <198258185+javohir0023@users.noreply.github.com>
2025-11-17 20:46:09 +0500

    xjm

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,9 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: '1-IMI',
+  description: 'Javoxir Xajiboyev tomonidan yaratilgan',
+  generator: 'XJM',
 }
 
 export default function RootLayout({
